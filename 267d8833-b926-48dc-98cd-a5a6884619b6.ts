deaefb1758862fb14919e3afd25dcf3851577cc9
Your Name <you@example.com>
2024-08-08 09:24:20 +0800

    1

---
+++ 267d8833-b926-48dc-98cd-a5a6884619b6.ts
@@ -0,0 +1,28 @@
+export default {
+  "entity": {
+    "id": "267d8833-b926-48dc-98cd-a5a6884619b6",
+    "name": "厦门大学校史资料 第四辑 1966-1987 上",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "厦门大学",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives16/main/267d8833-b926-48dc-98cd-a5a6884619b6.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "厦门大学校史资料 第四辑 1966-1987 上",
+        "authors": [
+          "厦门大学"
+        ],
+        "page_start": 1,
+        "page_end": 50,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "267d8833-b926-48dc-98cd-a5a6884619b6.pdf",
+  "resource_type": "book",
+  "version": 2
+}
---
+++ 97ad0b11-cdc0-4364-9347-a884e3eee82d.ts
@@ -0,0 +1,28 @@
+export default {
+  "entity": {
+    "id": "97ad0b11-cdc0-4364-9347-a884e3eee82d",
+    "name": "厦门大学校史资料 第四辑 1966-1987 中",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "厦门大学",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives16/main/97ad0b11-cdc0-4364-9347-a884e3eee82d.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "厦门大学校史资料 第四辑 1966-1987 中",
+        "authors": [
+          "厦门大学"
+        ],
+        "page_start": 1,
+        "page_end": 50,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "97ad0b11-cdc0-4364-9347-a884e3eee82d.pdf",
+  "resource_type": "book",
+  "version": 2
+}
