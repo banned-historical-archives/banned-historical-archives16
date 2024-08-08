75368b9aa575c96943e63081815588f660c14c48
Your Name <you@example.com>
2024-08-08 09:24:47 +0800

    1

---
+++ d3b9dabf-545b-46a2-b0da-19164bd0d271.ts
@@ -0,0 +1,28 @@
+export default {
+  "entity": {
+    "id": "d3b9dabf-545b-46a2-b0da-19164bd0d271",
+    "name": "厦门大学校史资料 第四辑 1966-1987 下",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "厦门大学",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives16/main/d3b9dabf-545b-46a2-b0da-19164bd0d271.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "厦门大学校史资料 第四辑 1966-1987 下",
+        "authors": [
+          "厦门大学"
+        ],
+        "page_start": 1,
+        "page_end": 6,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "d3b9dabf-545b-46a2-b0da-19164bd0d271.pdf",
+  "resource_type": "book",
+  "version": 2
+}
