846c25c15f3406e59a2465c360427b653c1e3565
Your Name <you@example.com>
2024-08-09 22:50:03 +0800

    1

---
+++ 15249607-0d42-4683-983b-ff50496f192c.ts
@@ -0,0 +1,28 @@
+export default {
+  "entity": {
+    "id": "15249607-0d42-4683-983b-ff50496f192c",
+    "name": "建国以来1949-1997客观、深入浅出的经济政治述评（下册）",
+    "internal": false,
+    "type": "pdf",
+    "official": false,
+    "author": "张家敏",
+    "files": "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives16/main/15249607-0d42-4683-983b-ff50496f192c.pdf"
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "建国以来1949-1997客观、深入浅出的经济政治述评（下册）",
+        "authors": [
+          "张家敏"
+        ],
+        "page_start": 1,
+        "page_end": 685,
+        "dates": []
+      }
+    ]
+  },
+  "parser_id": "automation",
+  "path": "15249607-0d42-4683-983b-ff50496f192c.pdf",
+  "resource_type": "book",
+  "version": 2
+}
