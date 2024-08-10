d6bad2e1842b275e3f08b1be8bab7e4d93db7689
Your Name <you@example.com>
2024-08-10 21:43:56 +0800

    1

---
+++ 3485bca6-43a9-498c-99e6-d4b4b08faf28.ts
@@ -0,0 +1,44 @@
+export default {
+  "entity": {
+    "id": "3485bca6-43a9-498c-99e6-d4b4b08faf28",
+    "name": "愤怒揭发批判李文亮篡党夺权的反革命罪行",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "张茂森",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives16/main/3485bca6-43a9-498c-99e6-d4b4b08faf28/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives16/main/3485bca6-43a9-498c-99e6-d4b4b08faf28/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives16/main/3485bca6-43a9-498c-99e6-d4b4b08faf28/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives16/main/3485bca6-43a9-498c-99e6-d4b4b08faf28/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives16/main/3485bca6-43a9-498c-99e6-d4b4b08faf28/5.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "愤怒揭发批判李文亮篡党夺权的反革命罪行",
+        "authors": [
+          "张茂森"
+        ],
+        "page_start": 1,
+        "page_end": 5,
+        "dates": [
+          {
+            "year": 1977,
+            "month": 12
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "3485bca6-43a9-498c-99e6-d4b4b08faf28",
+  "resource_type": "book",
+  "version": 2
+}
