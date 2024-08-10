a197ae003f3b997b18823e30621d1f318bb990e3
Your Name <you@example.com>
2024-08-10 22:21:34 +0800

    1

---
+++ 76336fc6-3d60-4fad-8433-a2cf29ba728c.ts
@@ -0,0 +1,45 @@
+export default {
+  "entity": {
+    "id": "76336fc6-3d60-4fad-8433-a2cf29ba728c",
+    "name": "彻底清算“四人帮”插手江苏的滔天罪行",
+    "internal": false,
+    "type": "imgs",
+    "official": false,
+    "author": "周德清",
+    "files": [
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives16/main/76336fc6-3d60-4fad-8433-a2cf29ba728c/1.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives16/main/76336fc6-3d60-4fad-8433-a2cf29ba728c/2.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives16/main/76336fc6-3d60-4fad-8433-a2cf29ba728c/3.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives16/main/76336fc6-3d60-4fad-8433-a2cf29ba728c/4.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives16/main/76336fc6-3d60-4fad-8433-a2cf29ba728c/5.jpg",
+      "https://raw.githubusercontent.com/banned-historical-archives/banned-historical-archives16/main/76336fc6-3d60-4fad-8433-a2cf29ba728c/6.jpg"
+    ]
+  },
+  "parser_option": {
+    "articles": [
+      {
+        "title": "彻底清算“四人帮”插手江苏的滔天罪行",
+        "authors": [
+          "周德清"
+        ],
+        "page_start": 1,
+        "page_end": 6,
+        "dates": [
+          {
+            "year": 1976,
+            "month": 12
+          }
+        ]
+      }
+    ],
+    "ocr": {
+      "use_onnx": true,
+      "det_model_dir": "./paddle/onnx/ch_PP-OCRv4_det_infer.onnx",
+      "rec_model_dir": "./paddle/onnx/ch_PP-OCRv4_rec_infer.onnx"
+    }
+  },
+  "parser_id": "automation",
+  "path": "76336fc6-3d60-4fad-8433-a2cf29ba728c",
+  "resource_type": "book",
+  "version": 2
+}
